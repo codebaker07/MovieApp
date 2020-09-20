89eecfcfa2ffff06065af956c612bfe9f1489eb0
stiwary0908 <stiwary0908@users.noreply.github.com>
2020-09-20 20:03:51 +0530

    Created initial Movie API

---
+++ Movie Booking/Back/Api/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+       
+
+       
+
+        public MovieController()
+        {
+           
+        }
+
+       
+    }
+}
